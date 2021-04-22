48deb3d905908fc7ad66f220289c02730d05f319
marcinkk1 <marcin383@interia.eu>
2021-04-22 13:26:51 +0200

    koniec zadania

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let bttn = document.getElementById('get-data');
+
+const getData = () => {
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+        .then(res => res.json())
+        .then(data => {
+            let pId = document.createElement('p');
+            let pUserId = document.createElement('p');
+            let pTitle = document.createElement('p');
+            let pBody = document.createElement('p');
+
+            pId.innerText = `Post ID: ${data.id}`;
+            pUserId.innerText = `User ID: ${data.userId}`;
+            pTitle.innerText = `Title: ${data.title}`;
+            pBody.innerText = `Body: ${data.body}`
+
+            document.body.appendChild(pId);
+            document.body.appendChild(pUserId);
+            document.body.appendChild(pTitle);
+            document.body.appendChild(pBody);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+bttn.addEventListener('click', getData);
